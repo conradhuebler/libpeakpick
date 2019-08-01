c2555ada30a24701f9336aa544c5ee1554ad9d28
Conrad Huebler <Conrad.Huebler@chemie.tu-freiberg.de>
2019-08-02 00:03:30 +0200

    fix crash ...

--- libpeakpick/spectrum.h
+++ libpeakpick/spectrum.h
@@ -1,20 +1,20 @@
 /*
  * <Spectrum Header file.>
  * Copyright (C) 2017  Conrad Hübler <Conrad.Huebler@gmx.net>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  */
 
 #pragma once
@@ -185,7 +185,10 @@ public:
 
     inline double Step() const
     {
-        return m_x[1] - m_x[0];
+        if (m_x.size() >= 2)
+            return m_x[1] - m_x[0];
+        else
+            return 0.0;
     }
 
     inline double XMin() const { return m_x[0]; }
